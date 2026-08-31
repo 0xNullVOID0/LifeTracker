4e88481ce0281d5729602ed0c9ac770ca03b55b7
0xNullVOID <0xNullVOID@proton.me>
2026-08-31 08:09:55 +0200

    Fix uncommitted new Endpoint class files

---
+++ LifeTracker/Endpoints/BuienradarEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using LifeTracker.Services;
+
+namespace LifeTracker.Endpoints;
+
+public static class BuienradarEndpoints
+{
+    public static IEndpointRouteBuilder MapBuienradarEndpoints(this IEndpointRouteBuilder routes)
+    {
+        routes.MapPost("/buienradar", async (BuienradarService service) =>
+        {
+            var data = await service.SyncStationMeasurement();
+            return data is not null ? Results.Ok(data) : Results.NoContent();
+        }).WithName("GetBuienradar").WithTags("Buienradar").WithSummary("Sync latest Buienradar station measurement")
+        .Produces(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status204NoContent);
+
+        return routes;
+    }
+}
---
+++ LifeTracker/Endpoints/RoomClimateEndpoints.cs
@@ -0,0 +1,22 @@
+﻿using LifeTracker.Services;
+using LifeTracker.Entities.ESP32;
+
+namespace LifeTracker.Endpoints;
+
+public static class RoomClimateEndpoints
+{
+    public static IEndpointRouteBuilder MapRoomClimateEndpoints(this IEndpointRouteBuilder routes)
+    {
+        // TODO fix handling route getting bad, wrong or not filled in json values
+        routes.MapPost("/api/room-climate", async (RoomClimateMeasurement body, ESP32Service service) =>
+        {
+            await service.SaveRoomClimate(body);
+            return Results.Ok(new { success = true });                                                                                             // TODO use device key instead of JWT for esp32?
+        }).WithName("PostRoomClimate").WithTags("RoomClimate").WithSummary("Ingest room climate measurements from ESP32 sensors").AllowAnonymous() // easier for now to not have ESP32 deal with JWT header tokens 
+        .Produces(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest);
+
+
+        return routes;
+    }
+}
